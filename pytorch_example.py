2a76e2970ee4e61748f2a4d1b1d37e87b4c4c2c1
Simon Butcher <s.butcher@qmul.ac.uk>
2021-09-20 11:22:03 +0100

    pytorch examples

---
+++ pytorch_example.py
@@ -0,0 +1,105 @@
+import torch
+import numpy as np
+
+# Tensors can be created directly from data. The data type is automatically inferred.
+data = [[1, 2],[3, 4]]
+x_data = torch.tensor(data)
+
+# Tensors can be created from NumPy arrays (and vice versa - see Bridge with NumPy).
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+# The new tensor retains the properties (shape, datatype)
+# of the argument tensor, unless explicitly overridden.
+x_ones = torch.ones_like(x_data) # retains the properties of x_data
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
+
+#shape is a tuple of tensor dimensions. In the functions below, it determines the dimensionality of the output tensor.
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+
+# Tensor attributes describe their shape, datatype, and the device on which they are stored.
+
+tensor = torch.rand(3,4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+
+# # We move our tensor to the GPU if available
+if torch.cuda.is_available():
+    tensor = tensor.to('cuda')
+
+# Standard numpy-like indexing and slicing:
+tensor = torch.ones(4, 4)
+print('First row: ', tensor[0])
+print('First column: ', tensor[:, 0])
+print('Last column:', tensor[..., -1])
+tensor[:,1] = 0
+print(tensor)
+
+# joining tensors
+
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(tensor)
+torch.matmul(tensor, tensor.T, out=y3)
+
+
+# This computes the element-wise product. z1, z2, z3 will have the same value
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
+
+# single element tensors
+
+agg = tensor.sum()
+agg_item = agg.item()
+print(agg_item, type(agg_item))
+
+# in place operations
+print(tensor, "\n")
+tensor.add_(5)
+print(tensor)
+
+# bridge with numpy
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+# A change in the tensor reflects in the NumPy array.
+
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+# NumPy array to Tensor
+
+n = np.ones(5)
+t = torch.from_numpy(n)
+
+# Changes in the NumPy array reflects in the tensor.
+
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
+
+
